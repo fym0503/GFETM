21eaddaed82b120f7609ec1d16f6ef9ac5e44695
Fan Yimin <48177486+fym0503@users.noreply.github.com>
2025-10-09 10:40:13 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 
 
-python main.py --mode train  --data_path ../data/buen18.h5ad --seq_path ../data/buen18_seq.h5 --checkpoint_path ../gfm_checkpoint/6-new-12w-0 --enc_drop 0.1 --num_topics 24 --seed 4 --epochs 8000 --emb_size 768 --rho_size 768 --t_hidden_size 1000 --output_path outputs/
+python main.py --mode train  --data_path ../data/buen18.h5ad --seq_path ../data/buen18_seq.h5 --full_data_path ../data/buen18.h5ad --checkpoint_path ../gfm_checkpoint/6-new-12w-0 --enc_drop 0.1 --num_topics 24 --seed 4 --epochs 8000 --emb_size 768 --rho_size 768 --t_hidden_size 1000 --output_path outputs/
